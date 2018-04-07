008e2ed65e84cade916957aea823a7d703c0e152
Arnaud Delaunoy <arnaud.delaunoy@student.uliege.be>
2018-04-07 14:02:07 +0200

    tmp

--- src/HTTP.java
+++ src/HTTP.java
@@ -6,21 +6,6 @@ import java.util.TimeZone;
 public abstract class HTTP {
 	protected static final float HTTP_VERSION = 1.1f;
 
-	/*
-	//Return a HTTP header for a given type and sive of message
-	static String getHeader(FileType type, int msgSize) {
-		String s = "";
-		
-        s+= "HTTP/"+HTTP_VERSION+" 200 OK\r\n";
-        s+="Date: "+getServerTime()+"\r\n";
-        s+="Content-Type: "+type.content_type+"\r\n";
-        s+="Content-Length: "+ msgSize +"\r\n";
-        s+="\r\n";
-        
-        return s;
-	}
-	*/
-
 	//Return the time in the HTTP format
 	protected static String getServerTime() {
 	    Calendar calendar = Calendar.getInstance();

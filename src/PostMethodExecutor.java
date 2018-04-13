d4a6bb8b011971654a5d21f51c10ae8815a2d7df
Arnaud Delaunoy <arnaud.delaunoy@student.uliege.be>
2018-04-13 09:00:02 +0200

    making post better

--- src/PostMethodExecutor.java
+++ src/PostMethodExecutor.java
@@ -36,39 +36,16 @@ public class PostMethodExecutor extends MethodExecutor {
                 throw new BadRequestException();
 
             Combination combi = new Combination(token.nextToken());
-
-            HTMLPage page = null;
-            try {
-                page = new HTMLPage();
-            } catch (IOException e) {
-                throw new BadRequestException();
-            }
-
-            if(this.cookie.isDuplicate(combi)){
-                return new HTTPRedirectionReply(HTMLPage.HTML_FILE);
-            }
-
+            
             combi.evaluate(this.cookie.getRightCombination());
             this.cookie.addTry(combi);
-
-            this.cookie.setUpHTMLPage(page);
-
-            System.out.println("generate code");
-            String replyBody = page.getHtmlCode();
-
-            replyHeaders.put(HTTPOption.DATE, new HTTPHeader(HTTPOption.DATE, getServerTime()));
-            replyHeaders.put(HTTPOption.CONTENT_TYPE, new HTTPHeader(HTTPOption.CONTENT_TYPE, FileType.HTML.getContentType()));
-            //Todo: la ligne en dessous est de la grosse merde juste en attendant de faire chunck encoding car ca va surement changer
-            // replyHeaders.put(HTTPOption.CONTENT_LENGTH, new HTTPHeader(HTTPOption.CONTENT_LENGTH, String.valueOf(replyBody.length() * 4)));
-            replyHeaders.put(HTTPOption.TRANSFER_ENCODING, new HTTPHeader(HTTPOption.TRANSFER_ENCODING, "chunked"));
-            replyHeaders.put(HTTPOption.CONTENT_ENCODING, new HTTPHeader(HTTPOption.CONTENT_ENCODING, "gzip"));
 
             //check for victory/deafeat
             if(combi.getResults()[0] == Combination.COMBI_LENGTH || cookie.getCurrentTry() == HTMLPage.LIVES) {
             	cookie.reset();
             }
 
-            return new HTTPReply(ReturnCode.OK, replyHeaders, replyBody);
+            return new HTTPRedirectionReply(HTMLPage.HTML_FILE);
         }
         catch (NoSuchElementException | BadFormatException | BadColorException e) {
             throw new BadRequestException();

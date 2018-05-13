de4e5b9f8aacda76e87650f5746b9b24f64c36ce
hokolomopo <adrien.minne@outlook.com>
2018-05-13 14:08:07 +0200

    Add test

--- zip/WebServer.java
+++ zip/WebServer.java
@@ -1,9 +1,13 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import javax.imageio.ImageIO;
 
 public class WebServer{
 
@@ -18,6 +22,8 @@ public class WebServer{
             int i = 0;
 
             boolean loop = true;
+            BufferedImage replyBody = ImageIO.read(new File("red.png"));
+            System.out.println(replyBody.getHeight());
             
             //create socket
             ServerSocket serverSocket = new ServerSocket(PORT);
